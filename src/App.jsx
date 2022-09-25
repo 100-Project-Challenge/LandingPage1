2a8826cfe3f3c4be18cc2b012fc8a1971aa8c445
AbeerAl-Rafati <abeerw.rafati@yahoo.com>
2022-09-25 23:20:10 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import styles from "./styles";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Billing />
+          <CardDeal />
+          <Business />
+          <Clients />
+          <CTA />
+          <Stats />
+          <Footer />
+          <Testimonials />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
